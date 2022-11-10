c76293a65ffbb7c8a5bf5ee37dd3530b8be091fa
olaniyi-ogungbe <olaniyiogungbe@gmail.com>
2022-11-10 20:23:06 +0100

    first commit

---
+++ backend/src/helpers/todosAcess.ts
@@ -0,0 +1,39 @@
+import * as AWS from 'aws-sdk'
+const AWSXRay = require ('aws-xray-sdk')
+import { DocumentClient } from 'aws-sdk/clients/dynamodb'
+// import { createLogger } from '../utils/logger'
+import { TodoItem } from '../models/TodoItem'
+// import { TodoUpdate } from '../models/TodoUpdate';
+
+const XAWS = AWSXRay.captureAWS(AWS)
+
+// const logger = createLogger('TodosAccess')
+
+const todosTable = process.env.TODOS_TABLE
+const docClient: DocumentClient = createDynamoDBClient()
+
+
+// // TODO: Implement the dataLayer logic
+export async function createTodo(todo : TodoItem) : Promise<TodoItem> {
+    
+    await docClient
+        .put({
+            TableName: todosTable,
+            Item: todo
+        })
+        .promise()
+
+    return todo
+}
+
+function createDynamoDBClient() {
+    if (process.env.IS_OFFLINE) {
+      console.log( )
+      return new XAWS.DynamoDB.DocumentClient({
+        region: 'localhost',
+        endpoint: 'http://localhost:8000'
+      })
+    }
+  
+    return new XAWS.DynamoDB.DocumentClient()
+  }
